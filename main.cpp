fbe3884ad6c5be04ed03949079ccbb9ea942861c
Ozatooo <61974211+Ozatooo@users.noreply.github.com>
2021-03-12 14:16:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main()
     plik.open("dane_obrazki.txt", ios::in);
 
     int ile = 0;
-    int dobry = 0;
+    char dobry;
     int poprawny = 0;
     int nienaprawialny = 0;
     int naprawialny = 0;
@@ -20,9 +20,12 @@ int main()
     while(!plik.eof()) {
 
         string obraz[21];
+        zlakolumna=0;
+        zlywiersz=0;
 
         for(int i=0;i<21;i++) {
             plik >> obraz[i];
+            
         }
 
         for(int i=0;i<20;i++) {
@@ -34,23 +37,23 @@ int main()
 			}
 			if(j==19)
 			{
-			  if(ile%2==0)
+			  if(ile%2==1)
         	{
-        		dobry=0;
+        		dobry='1';
 			}
 			else
 			{
-				dobry=1;
+				dobry='0';
 			}
 			
-			if(obraz[i][20]!=dobry)
+			if(dobry!=obraz[i][20])
 			{
 				zlywiersz++;
 			}
 			ile=0;
 			}
 		}
-        }
+    }
         
             for(int i=0;i<20;i++) {
             for(int j=0; j<20; j++)
@@ -61,16 +64,16 @@ int main()
 			}
 			if(j==19)
 			{
-				if(ile%2==0)
+				if(ile%2==1)
 	        	{
-	        		dobry=0;
+	        		dobry='1';
 				}
 				else
 				{
-					dobry=1;
+					dobry='0';
 				}
 				
-				if(obraz[i][20]!=dobry)
+				if(obraz[20][i]!=dobry)
 				{
 					zlakolumna++;
 				}
@@ -79,37 +82,28 @@ int main()
 		}
         }
         
-        
-        
-    
-        
+
     if(zlakolumna>1 || zlywiersz>1)
     {
         nienaprawialny++;
 
     }
-    else if(zlakolumna<=1 || zlywiersz<=1)
+    else if(zlakolumna>0 || zlywiersz>0)
     {
         naprawialny++;
 
     }
-    else
+    else if(zlakolumna==0 || zlywiersz==0)
     {
         poprawny++;
 
     }
-			    
-        zlakolumna=0;
-        zlywiersz=0;
-
-        
-        	
-        
+			
 
     }
-        cout<<"poprawnych"<<poprawny<<endl;
-        cout<<"nie naprawialny"<<nienaprawialny<<endl;
-        cout<<"naprawilanych"<<naprawialny;
+        cout<<"poprawnych "<<poprawny<<endl;
+        cout<<"nie naprawialny "<<nienaprawialny<<endl;
+        cout<<"naprawilanych "<<naprawialny;
 
     return 0;
 }
